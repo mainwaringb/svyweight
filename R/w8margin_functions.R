b0f571db7e3a53d29a834b48eff1a3d488c8bc76
Ben Mainwaring <mainwaringb@gmail.com>
2022-01-17 17:49:23 +0300

    Split quality functions into separate file

--- R/w8margin_functions.R
+++ R/w8margin_functions.R
@@ -413,38 +413,6 @@ impute_w8margin <- function(w8margin, observed, weights = NULL, rebase = TRUE){
 }
 
 
-## ==== MISCELLANEOUS FUNCTIONS ====
-
-#' Effective Sample Size and Weighting Efficiency
-#' @description Computes Kish's effective sample size or weighting efficiency for a
-#'   \code{\link[survey]{svydesign}} object. 
-#' @param design An \code{\link[survey]{svydesign}} object, presumably with
-#'   design or post-stratification weights.
-#' @details Kish's effective sample size is a frequently-used, general metric to
-#'   indicate how much uncertainty and error increase due to weighting. 
-#'   Effective sample size is calculated as \code{sum(weights(design))^2 / sum(weights(design)^2)}. 
-#'   Weighting efficiency is \code{eff_n(design) / sum(weights(design))}.
-#' @details While weighting efficency and effective sample size are frequently use,
-#'  they are less valid than the standard errors produced by
-#'   \code{\link[survey]{svymean}} and related functions from the {survey}
-#'   package. In particular, they ignore clustering and stratification in 
-#'   sample designs, and covariance between weighting variables and outcome variables.
-#'   As such, these metrics should be used with caution
-#' @example inst/examples/eff_n_examples.R
-#' @export
-eff_n <- function(design){
-  myweights <- weights.survey.design(design)
-  eff_n <- (sum(myweights) ^ 2) / (sum(myweights ^ 2))
-  return(eff_n)
-}
-
-#' @rdname eff_n
-#' @export
-weight_eff <- function(design){
-  out <- eff_n(design) / sum(weights.survey.design(design))
-  return(out)
-}
-
 
 ## ==== INTERNAL FUNCTIONS ====
 
